cc8f67b626c25faf9c7589dd23bd90177f3a10d1
eziaowonder <eziaowonder@gmail.com>
2022-03-10 18:04:50 +0000

    修正

--- layers/SE_Block.py
+++ layers/SE_Block.py
@@ -1,25 +1,5 @@
-
-from collections import OrderedDict
-
 from torch import nn
 
-# class SELayer(nn.Module):
-#     def __init__(self, Channel, reduction=16) -> None:
-#         super(SELayer, self).__init__()
-#         self.gobal_pooling = nn.AdaptiveAvgPool2d(1)
-#         self.se = nn.Sequential(OrderedDict([
-#           ('fc', nn.Linear(Channel, Channel // reduction, bias=False)),
-#           ('relu', nn.ReLU(inplace=True)),# save memory
-#           ('fc', nn.Linear(Channel // reduction, Channel, bias=False)),
-#           ('sigmoid', nn.Sigmoid()),
-#         ]))
-        
-
-#     def forward(self, x):
-#         b, c, _, _ = x.size() # batch, channel
-#         output = self.gobal_pooling(x).view(b,c)
-#         output = self.se(output).view(b,c)
-#         return x * output.expand_as(x)
 
 class SE_Block(nn.Module):
     "credits: https://github.com/moskomule/senet.pytorch/blob/master/senet/se_module.py#L4"
